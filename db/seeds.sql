ed65c84d7b3365a7b3830e2142bc4a740429424a
Preston Cropp <pkcropp@gmail.com>
2018-03-13 21:15:29 -0500

    updated packs + gitignore

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO vegetables (vege_name) VALUES ('Lettuce');
+INSERT INTO vegetables (vege_name) VALUES ('Tomato');
+INSERT INTO vegetables (vege_name) VALUES ('Onion');
+INSERT INTO vegetables (vege_name) VALUES ('Pickles');
